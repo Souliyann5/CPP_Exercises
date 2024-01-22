9698359e61c71a7430287da15199d1bbb04854df
THEPHARATH_Souliyann <souliyann.thepharath@edu.univ-eiffel.fr>
2024-01-22 15:02:00 +0100

    tp1 - ex2 - q6

--- tp1/ex2/main.cpp
+++ tp1/ex2/main.cpp
@@ -1,12 +1,33 @@
 #include "Person.cpp"
 #include <iostream>
+#include <vector>
+
 
 int main()
 {
-    Person p;
-    p.set_first_name("Palluche");
-    p.set_surname("La Faluche");
-    std::cout << "La personne s'appelle " << p.get_first_name() << " " << p.get_surname() << std::endl;
+    int nb_person;
+    std::string first_name;
+    std::string surname;
+    std::cin >> nb_person;
+    std::vector<Person> vec(nb_person);
+
+    for (auto& p: vec)
+    {
+        std::cout << "Prenom" << std::endl;
+        std::cin >> first_name;
+        p.set_first_name(first_name);
+
+        std::cout << "Nom" << std::endl;
+        std::cin >> surname;
+        p.set_surname(surname);
+    }
+
+    std::cout << "Les personnes sont : " << std::endl;
+
+    for (auto p: vec)
+    {
+        std::cout << p.get_first_name() << " " << p.get_surname() << std::endl;
+    }
 
     return 0;
 }
