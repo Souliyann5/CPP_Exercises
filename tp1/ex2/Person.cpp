3373bb6fda14a0d6b97dac8c6b3ebe0c4679d058
THEPHARATH_Souliyann <souliyann.thepharath@edu.univ-eiffel.fr>
2024-01-22 15:19:07 +0100

    tp1 - ex2 - q6

--- tp1/ex2/Person.cpp
+++ tp1/ex2/Person.cpp
@@ -1,7 +1,15 @@
 #include <string>
+
 class Person
 {
 public:
+
+    Person(){}
+    Person(std::string first_name,std::string surname)
+        : _first_name { first_name }
+        , _surname { surname }
+        {}
+
     void set_first_name(const std::string& first_name) { _first_name = first_name; }
     void set_surname(const std::string& surname) { _surname = surname; }
     std::string get_first_name() const {return _first_name;}
--- tp1/ex2/main.cpp
+++ tp1/ex2/main.cpp
@@ -8,18 +8,31 @@ int main()
     int nb_person;
     std::string first_name;
     std::string surname;
+
+    std::cout << "Nombre de personnes : " << std::endl;
     std::cin >> nb_person;
     std::vector<Person> vec(nb_person);
 
+    // for (auto& p: vec)
+    // {
+    //     std::cout << "Prenom" << std::endl;
+    //     std::cin >> first_name;
+    //     p.set_first_name(first_name);
+
+    //     std::cout << "Nom" << std::endl;
+    //     std::cin >> surname;
+    //     p.set_surname(surname);
+    // }
+
     for (auto& p: vec)
     {
-        std::cout << "Prenom" << std::endl;
-        std::cin >> first_name;
-        p.set_first_name(first_name);
+        std::cout << "Prenom + Nom" << std::endl;
+        std::cin >> first_name >> surname;
 
-        std::cout << "Nom" << std::endl;
-        std::cin >> surname;
-        p.set_surname(surname);
+        // Person person(first_name,surname);
+        // vec.push_back(person);
+
+        vec.emplace_back(first_name, surname);
     }
 
     std::cout << "Les personnes sont : " << std::endl;
